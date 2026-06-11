56e72bb8382289e7afde16941a81a2812788b358
Akbar Iskanderov <iskanderovofficial@gmail.com>
2026-06-11 17:54:42 +0500

    done

--- lib/api/couriers.ts
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from "./client";
-import type { CourierProfile } from "@/types";
-
-export async function getCouriers(accessToken?: string) {
-  return apiRequest<CourierProfile[]>("/couriers", {
-    method: "GET",
-    accessToken,
-  });
-}
